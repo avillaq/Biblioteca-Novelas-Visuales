f95ebd35da352beba37e5039a2209c87fcd57723
avillaq <villafuertequispealex@gmail.com>
2024-02-19 17:57:05 -0500

    Add Post model to VNDisplay app

--- VNDisplay/models.py
+++ VNDisplay/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
+    full_url = models.URLField(max_length=250)
+    image_url = models.URLField(max_length=250)
+    description = models.TextField()
+    categories = models.CharField(max_length=250)
+    date = models.DateField()
+
+    class Meta:
+        ordering = ['-date'] #IMPORTANT: Sort by date date in descending order
+        db_table = "post"
+
+    def __str__(self):
+        return self.title
+    
+    #IMPORTANT This method will return the canonical URL for a post
+    def get_absolute_url(self):
+        return reverse('VNDisplay:novel_detail',
+                       args=[self.date.year,
+                            self.date.month,
+                            self.date.day,
+                            self.slug])
+
+
