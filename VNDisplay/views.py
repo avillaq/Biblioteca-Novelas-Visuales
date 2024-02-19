36aac4adc15e1a891c18fd95e890247e976fbc4c
avillaq <villafuertequispealex@gmail.com>
2024-02-19 18:51:09 -0500

    Add Post model and save scraped data

--- VNDisplay/views.py
+++ VNDisplay/views.py
@@ -1,18 +1,31 @@
+import re
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from VNDisplay.Post import VN_Scraper, Post, Post_Android
+from VNDisplay.Post import VN_Scraper
+from VNDisplay.models import Post
 
 scraper = VN_Scraper()
 current_posts = []
 
-# Create your views here.
-#index view
+
 def home(request):
-    posts = scraper.get_section("inicio")
+    posts = scraper.get_section("inicio")[0:1]
+    p = posts[0]
+    
+   
+    post = Post(title=p.title, slug=create_slug(p.full_url), full_url=p.full_url, image_url=p.image_url, description=p.description, categories=p.labels, date=p.date)
+    post.save()
 
     return render(request, 'home.html', {'posts': posts})
 
 
+def create_slug(full_url):
+    match = re.search(r"/([\w-]+)\.html", full_url)
+    if match:
+        match = match.group(1)
+    else:
+        raise ValueError(f"Invalid URL: {full_url}")
+    return match
 
 
 def novel_detail(request, year, month, title):
