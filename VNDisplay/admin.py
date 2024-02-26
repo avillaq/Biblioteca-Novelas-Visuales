a5e8889998bb8885172e46581a1568c17b1af32f
avillaq <villafuertequispealex@gmail.com>
2024-02-26 18:06:35 -0500

    Add Android_Post and Type models to admin interface

--- VNDisplay/admin.py
+++ VNDisplay/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Android_Post, Type
 
 # admin.site.register(Post) . if you want to register the model with the default admin interface
 
@@ -13,5 +13,17 @@ class PostAdmin(admin.ModelAdmin):
 
 @admin.register(Category) # if you want to register the model with the custom admin interface
 class CategoryAdmin(admin.ModelAdmin): 
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Android_Post)
+class Android_PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'type']
+    list_filter = ['type']
+    search_fields = ['title']
+    ordering = ['title']
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
     list_display = ['name']
     search_fields = ['name']
