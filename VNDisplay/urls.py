34ab2b4b8282f82e77d062555038721a6602b9c8
avillaq <villafuertequispealex@gmail.com>
2024-02-26 12:45:19 -0500

    Add Android APK and Kirikiroid2 paths to URLs

--- VNDisplay/urls.py
+++ VNDisplay/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', view=home, name='home'),
     path('directorio', view=directory, name='directory'),
     path('android/', view=android, name='android'),
+    path('android/apk', view=android_apk, name='android_apk'),
+    path('android/kirikiroid2', view=android_kirikiroid2, name='android_kirikiroid2'),
+
     path('novel/<int:year>/<int:month>/<int:day>/<slug:title>/', novel_detail, name='novel_detail'),
 
 
