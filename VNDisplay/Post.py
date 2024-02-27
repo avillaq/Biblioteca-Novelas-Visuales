8b371170b9285d804a6c97e23d30281588c00b64
avillaq <villafuertequispealex@gmail.com>
2024-02-27 18:03:56 -0500

    Fix internet connection error handling in VN_Scraper class

--- VNDisplay/Post.py
+++ VNDisplay/Post.py
@@ -284,8 +284,14 @@ class VN_Scraper:
     def get_apk_section(self) -> list[Post_Android]:
 
         url = self.sections["apk"]
-        header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.2151.97'}
-        response = requests.get(url, headers=header)
+
+        try:
+            header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.2151.97'}
+            response = requests.get(url, headers=header)
+        except requests.exceptions.ConnectionError as e:
+            print("Internet connection error. Please check your connection.")
+            return []
+        
         soup = BeautifulSoup(response.content, 'html.parser')
 
         post = soup.find('div', class_='post-body')
@@ -366,8 +372,14 @@ class VN_Scraper:
     ############################# The kirikiroid2 section is different from the others, so we need to scrap it differently #############################
     def get_kirikiroid2_section(self) -> list[Post_Android]:
         url = self.sections["kirikiroid2"]
-        header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.2151.97'}
-        response = requests.get(url, headers=header)
+        
+        try:
+            header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.2151.97'}
+            response = requests.get(url, headers=header)
+        except requests.exceptions.ConnectionError as e:
+            print("Internet connection error. Please check your connection.")
+            return []
+        
         soup = BeautifulSoup(response.content, 'html.parser')
 
         post = soup.find('div', class_='post-body')
@@ -414,8 +426,14 @@ class VN_Scraper:
         
     def get_kirikiroid2_emulator(self) -> str:
         url = self.sections["kirikiroid2"]
-        header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.2151.97'}
-        response = requests.get(url, headers=header)
+
+        try:
+            header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.2151.97'}
+            response = requests.get(url, headers=header)
+        except requests.exceptions.ConnectionError as e:
+            print("Internet connection error. Please check your connection.")
+            return ""
+        
         soup = BeautifulSoup(response.content, 'html.parser')
 
         post = soup.find('div', class_='post-body')
