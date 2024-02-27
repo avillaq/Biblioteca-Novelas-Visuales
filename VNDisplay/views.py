9c6ca2f2a3ceb600200dbae680833a781e858bff
avillaq <villafuertequispealex@gmail.com>
2024-02-26 23:32:11 -0500

    Update android_kirikiroid2 view to fetch posts from scraper

--- VNDisplay/views.py
+++ VNDisplay/views.py
@@ -76,7 +76,8 @@ def android_apk(request):
     return render(request, 'android_apk.html', {'android_posts': android_posts})
 
 def android_kirikiroid2(request):
-    return render(request, 'android_kirikiroid2.html')
+    android_posts = scraper.get_kirikiroid2_section()
+    return render(request, 'android_kirikiroid2.html', {'android_posts': android_posts})
 
 def verify_new_posts():
     if not Post.objects.exists():  #IMPORTANT: If there are no posts in the database. IT WILL TAKE A FEW MINUTES 
