fb5ddb6eb6d6f73d8dae73a72d1550c490f3fc50
avillaq <villafuertequispealex@gmail.com>
2024-02-27 18:53:18 -0500

    Add search functionality and update Android URL

--- VNDisplay/urls.py
+++ VNDisplay/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from VNDisplay.views import home, directory, novel_detail, android, android_apk, android_kirikiroid2
+from VNDisplay.views import home, directory, novel_detail, android, android_apk, android_kirikiroid2, search
 
 app_name = 'VNDisplay'
 urlpatterns = [
     path('', view=home, name='home'),
     path('directorio', view=directory, name='directory'),
-    path('android/', view=android, name='android'),
+    path('search', view=search, name='search'),
+    path('android', view=android, name='android'),
     path('android/apk', view=android_apk, name='android_apk'),
     path('android/kirikiroid2', view=android_kirikiroid2, name='android_kirikiroid2'),
 
