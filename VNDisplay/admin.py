20a6e6a88d72304370638e890e029a1f544eeb3b
avillaq <villafuertequispealex@gmail.com>
2025-01-19 23:38:44 -0500

    Refactor admin interface for Post, Category, Android_Post, and Type models to improve display fields and filtering options

--- VNDisplay/admin.py
+++ VNDisplay/admin.py
@@ -1,35 +1,26 @@
 from django.contrib import admin
-from .models import Post, Category, Android_Post, Type, PostDetail
-
-# admin.site.register(Post) . if you want to register the model with the default admin interface
+from .models import Post, Category, Android_Post, Type
 
 @admin.register(Post) # if you want to register the model with the custom admin interface
 class PostAdmin(admin.ModelAdmin): 
-    list_display = ['title', 'slug', 'date']
-    list_filter = ['title', 'date']
-    search_fields = ['title']
-    prepopulated_fields = {'slug': ('title',)}
-    ordering = ['date']
+    list_display = ["title", "categories", "publicaction_date", "update_date"]
+    list_filter = ["title", "categories", "publicaction_date", "update_date"]
+    search_fields = ["title"]
+    ordering = ["publicaction_date", "update_date"]
 
-@admin.register(Category) # if you want to register the model with the custom admin interface
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin): 
-    list_display = ['name']
-    search_fields = ['name']
+    list_display = ["name"]
+    search_fields = ["name"]
 
 @admin.register(Android_Post)
 class Android_PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'type']
-    list_filter = ['type']
-    search_fields = ['title']
-    ordering = ['id']
+    list_display = ["title", "type"]
+    list_filter = ["type"]
+    search_fields = ["title"]
+    ordering = ["id"]
 
 @admin.register(Type)
 class TypeAdmin(admin.ModelAdmin):
-    list_display = ['name', 'resource']
-    search_fields = ['name']
-
-@admin.register(PostDetail)
-class PostDetailAdmin(admin.ModelAdmin):
-    list_display = ['post', 'synopsis']
-    search_fields = ['post']
-    ordering = ['post']
+    list_display = ["name", "resource"]
+    search_fields = ["name"]
