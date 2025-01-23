f08f37832d80768209d485fdd58942e631b374d9
avillaq <villafuertequispealex@gmail.com>
2025-01-22 21:12:41 -0500

    Add production host to ALLOWED_HOSTS in settings

--- VisualNovel/settings.py
+++ VisualNovel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d=uctpn9r_bik36%)^$iv@7%&x^k@2(=%ire!+uzq&_#czqsl1
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "visual-novel-library.up.railway.app"]
 
 
 # Application definition
